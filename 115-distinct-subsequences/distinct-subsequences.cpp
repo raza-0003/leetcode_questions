f388790c217029aa92562c94dd8822a472f95d4f
Faizan Raza <faizanraza2308@gmail.com>
2025-01-14 09:13:20 +0530

    Time: 2793 ms (5.09%) | Memory: 422.3 MB (6.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
+    int f(int ind1,int ind2,string s, string t,vector<vector<int>>&dp){
+        if(ind2<0) return 1;
+        if(ind1<0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(s[ind1] == t[ind2]){
+            return dp[ind1][ind2] = (f(ind1-1,ind2-1,s,t,dp) + f(ind1-1,ind2,s,t,dp));
+        }
+        return dp[ind1][ind2] = f(ind1-1,ind2,s,t,dp);
+    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
         // create dp table
-        vector<unsigned long long>dp(m+1,0);
-        dp[0]=1;
-        for(int ind1=1;ind1<=n;ind1++){
-            for(int ind2=m;ind2>0;ind2--){
-                if(s[ind1-1]== t[ind2-1]){
-                    dp[ind2] += dp[ind2-1];
-                    
-                }
-            }
-        }
-        return dp[m];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,t,dp);
     }
 };
