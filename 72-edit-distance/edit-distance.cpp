752195f521df3af58aae3a81f9b03b6fe5193002
Faizan Raza <faizanraza2308@gmail.com>
2025-01-15 10:31:57 +0530

    Time: 10 ms (34.92%) | Memory: 13 MB (53.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int f(int ind1,int ind2,string word1,string word2,vector<vector<int>>&dp){
-            if(ind1<0) return ind2 + 1; // insert remaining character of word2
-            if(ind2<0) return ind1 + 1; // delete remaining character of word1
-            if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-            if(word1[ind1] == word2[ind2]){
-                return dp[ind1][ind2] =  0 + f(ind1-1,ind2-1,word1,word2,dp);
-            }
-            return dp[ind1][ind2] =  1 + min({f(ind1-1,ind2,word1,word2,dp) // delete
-                    ,f(ind1,ind2-1,word1,word2,dp)         // insert
-                    ,f(ind1-1,ind2-1,word1,word2,dp)});    // replace
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        if(n==0) return m;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        if (n == 0) return m;
+        if (m == 0) return n;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(word1[ind1-1] == word2[ind2-1]){
+                    dp[ind1][ind2] =  0 + dp[ind1-1][ind2-1];
+                }
+                else{
+                    dp[ind1][ind2] =  1 + min({dp[ind1-1][ind2]// delete
+                                        ,dp[ind1][ind2-1]         // insert
+                                        ,dp[ind1-1][ind2-1]});    // replace
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
