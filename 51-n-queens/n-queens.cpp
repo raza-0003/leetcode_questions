b2f425180430f1359745bdd26be49edfad318c83
Faizan Raza <faizanraza2308@gmail.com>
2025-02-18 13:35:47 +0530

    Time: 4 ms (38.13%) | Memory: 12.8 MB (18.23%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string>board,int n){
-        // checking the upper diagonal
-        int duprow = row;
-        int dupcol = col;
-        while(row>=0 && col>=0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            row--;
-            col--;
-        }
-        // checking for the left side
-        col = dupcol;
-        row = duprow;
-        while(col>=0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        row = duprow;
-        col = dupcol;
-        // checking for the lower_left diagonal
-        while(row<n && col>=0){
-            if(board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n){
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftRow,vector<int>&upperDiag,vector<int>lowerDiag,int n){
         if(col == n){
             ans.push_back(board);
             return;
         }
         for(int row = 0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row] == 0 && lowerDiag[row+col] == 0 && upperDiag[n-1+col-row] == 0 ){
                 board[row][col] = 'Q';
-                solve(col+1,board,ans,n);
+                leftRow[row] = 1;
+                lowerDiag[row+col] = 1;
+                upperDiag[n-1+col-row] = 1;
+                solve(col+1,board,ans,leftRow,upperDiag,lowerDiag,n);
                 // backktracking 
                 board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiag[row+col] = 0;
+                upperDiag[n-1+col-row] = 0;
+
             }
         }
     }
@@ -50,7 +28,8 @@ public:
         for(int i=0;i<n;i++){
             board[i] = s;
         }
-        solve(0,board,ans,n);
+        vector<int>leftRow(n,0),upperDiag(2*n-1,0),lowerDiag(2 *n-1,0);
+        solve(0,board,ans,leftRow,upperDiag,lowerDiag,n);
         return ans;
         
     }
