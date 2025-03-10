f3eaead2edf81cec8f3057c2603689fa463d2b0c
Faizan Raza <faizanraza2308@gmail.com>
2025-03-10 18:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (37.39%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,16 +3,19 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         if(n<=0) return "";
+        // alhabatically string ko sort akar diya
         sort(strs.begin(),strs.end());
         string first = strs[0];
         string last = strs[n-1];
         int i=0;
+        // comparing first and the last element of the array of the string
         while(i<first.size() && i<last.size() && first[i] == last[i]){
             i++;
         }
+
         string ans="";
         for(int a=0;a<i;a++){
-            ans += first[a];
+            ans += last[a];
         }
         return ans;
     }
