49606837a5e205ec76948a3cf0450a520d0b852f
Faizan Raza <faizanraza2308@gmail.com>
2025-03-10 19:20:00 +0530

    Time: 4 ms (17.99%) | Memory: 10.1 MB (35.55%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // transforming into the lower case
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        string result;
+        for(char ch:s){
+            if(isalnum(ch)){
+                result += ch;
+            }
+        }
+        int n = result.size();
+        int start = 0;
+        int end = n-1;
+        int cnt = 0;
+        bool ans = true;
+        while(start < end){
+            if(result[start] != result[end]){
+                ans = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        return ans;
+
+
+        
+    }
+};
