0db51bbcb6c1b45f52e3abcf0344750d6cfb7751
Faizan Raza <faizanraza2308@gmail.com>
2025-03-11 12:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (26.54%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.cpp
+++ 2226-rings-and-rods/rings-and-rods.cpp
@@ -3,15 +3,18 @@ public:
     int countPoints(string rings) {
         int n = rings.size();
         unordered_map<int,vector<char>>mpp;
+        // for every rod mapping colour 
         for(int i=1;i<n;i+=2){
             mpp[rings[i]].push_back(rings[i-1]);
         }
         int cnt = 0;
+        // checking for every rod having the coour r,g,b
         for(auto it:mpp){
             if(find(it.second.begin(),it.second.end(),'R') != it.second.end() 
              && find(it.second.begin(),it.second.end(),'G') != it.second.end()
              && find(it.second.begin(),it.second.end(),'B') != it.second.end())
              {
+                // increase the count by 1
                 cnt++;
             }
         }
