44a05a3e06bad2ffb2485fac1f1698d0245656a3
Faizan Raza <faizanraza2308@gmail.com>
2025-04-01 23:21:34 +0530

    Time: 7 ms (54.53%) | Memory: 119.7 MB (86.16%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    long long maxi(vector<vector<int>>& questions,int ind,vector<long long>&dp){
-        // writing down the base case
-        if(ind >= questions.size()){
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        // not picking the element 
-        long long  notPick = maxi(questions,ind+1,dp);
-        long long pick = questions[ind][0] + maxi(questions,ind+questions[ind][1]+1,dp); 
-        return dp[ind] = max(pick,notPick);
-        
-    }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
         vector<long long>dp(n+1,0);
