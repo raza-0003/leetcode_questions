54e3cc9c2b8da8f7c2fc6dafc8ef6d661df0e554
Faizan Raza <faizanraza2308@gmail.com>
2025-04-01 23:21:17 +0530

    Time: 4 ms (75.29%) | Memory: 119.8 MB (58.89%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -14,9 +14,17 @@ public:
     }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long>dp(n,-1);
-        long long maxSum = maxi(questions,0,dp);
-        return maxSum;
+        vector<long long>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            long long pick = questions[i][0];
+            int nextInd = i + questions[i][1] + 1;
+            if(nextInd<n){
+                pick += dp[nextInd];
+            }
+            long long notPick = dp[i+1];
+            dp[i] = max(pick,notPick);
+        }
+        return dp[0];
         
     }
 };
