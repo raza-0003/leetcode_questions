85f75b2f8c1208fa5bf9d4b190a99596fb3601c7
Faizan Raza <faizanraza2308@gmail.com>
2025-05-20 22:32:09 +0530

    Time: 18 ms (33.30%) | Memory: 14.3 MB (22.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,12 +4,15 @@ public:
         int n = s.size();
         int left = 0;
         int maxLen = 0;
+        // creating the unordered_set to evaluate the distinct digit
         unordered_set<char>charSet;
         for(int right = 0; right<n;right++){
+            // keep checking until we got the distict charsets
             while(charSet.find(s[right]) != charSet.end()){
                 charSet.erase(s[left]);
                 left++;
             }
+            //adding distinct char into the charSet
             charSet.insert(s[right]);
             maxLen = max(maxLen,right-left+1);
         }
