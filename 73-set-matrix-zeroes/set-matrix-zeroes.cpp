791ed96b533c2d31b1b771f08c4e20e4686509d0
Faizan Raza <faizanraza2308@gmail.com>
2025-05-21 13:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -13,9 +13,11 @@ public:
                 }
             }
         }
-        // making all the arrays to zero
+        // making all the rows to zero
         for(int i=0;i<n;i++){
+            // travesing all the rows array and find if any element is zero
             if(row[i]==0){
+                // for that particulat row do all coloumn to zero
                 for(int j=0;j<m;j++){
                     matrix[i][j] = 0;
                 }
@@ -23,7 +25,9 @@ public:
         }
         // making all the column to zero
         for(int i=0;i<m;i++){
+            // traversing all the column arrays and check if any col element is zero
             if(cols[i] == 0){
+                // for that particular col do all row to zero
                 for(int j=0;j<n;j++){
                     matrix[j][i] = 0;
                 }
