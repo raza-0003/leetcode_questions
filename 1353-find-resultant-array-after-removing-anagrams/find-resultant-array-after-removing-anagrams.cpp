3febd787f94cd9e86b4497e0c30f9b19ef7afdbb
Faizan Raza <faizanraza2308@gmail.com>
2025-10-13 12:29:33 +0530

    Time: 3 ms (48.84%) | Memory: 16.2 MB (54.91%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -3,13 +3,9 @@ public:
     bool isAnagram(string s1,string s2){
         if(s1 == s2) return true;
         if(s1.size() != s2.size()) return false;
-        vector<int>freq1(26,0);
-        vector<int>freq2(26,0);
-        for(int i=0;i<s1.size();i++){
-            freq1[s1[i]-'a']++;
-            freq2[s2[i]-'a']++;
-        }
-        return freq1 == freq2;
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        return s1==s2;
     }
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string>ans;
