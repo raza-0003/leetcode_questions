ef5318db50446773ac6afe455409f594bd38077e
Faizan Raza <faizanraza2308@gmail.com>
2025-12-16 20:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.72%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == 1){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == 2){
+                ans.push_back(nums[i]);
+            }
+        }
+        nums = ans;
+    }
+};
