d460458021adba4d0d10dd6a1b63de1945576e4a
Faizan Raza <faizanraza2308@gmail.com>
2026-01-16 15:40:23 +0530

    Time: 14 ms (93.41%) | Memory: 9.2 MB (89.72%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,7 +4,6 @@ public:
         int n = text1.size();
         int m = text2.size();
         vector<int>prev(m+1,0),curr(m+1,0);
-        for(int j=0;j<=m;j++) prev[j] = 0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1[i-1] == text2[j-1]){
