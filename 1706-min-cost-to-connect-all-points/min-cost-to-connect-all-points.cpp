1e11193b50d91c901dad7f5185b8d2bf60e98404
Faizan Raza <faizanraza2308@gmail.com>
2026-01-28 21:06:02 +0530

    Time: 620 ms (28.40%) | Memory: 109.9 MB (43.34%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
-    typedef pair<int,int>P;
+    int find(int i,vector<int>&parent){
+        if(i == parent[i]){
+            return i;
+        }
+        return find(parent[i],parent);
+    }
+    void Union(int x, int y,vector<int>&parent){
+        int x_parent = find(x,parent);
+        int y_parent = find(y,parent);
+        if(x_parent != y_parent){
+            // kisi ek ko papa bana do --> let say y ko bana diya
+            parent[x_parent] = y_parent;
+        }
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<vector<pair<int,int>>>adj(n);
+        vector<int>parent(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+        vector<pair<int, pair<int,int>>>edges;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
-                adj[i].push_back({j,dist});
-                adj[j].push_back({i,dist});
+                int x1 = points[i][0];
+                int x2 = points[j][0];
+                int y1 = points[i][1];
+                int y2 = points[j][1];
+                int d = abs(x1-x2) + abs(y1-y2);
+                edges.push_back({d,{i,j}});
+                edges.push_back({d,{j,i}});
             }
         }
-        vector<bool>visited(n,false);
-        priority_queue<P,vector<P>,greater<P>>pq;
-        pq.push({0,0});
-        int min_dist = 0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int dist = it.first;
-            int node = it.second;
-            if(visited[node]){
-                continue;
-            }
-            visited[node] = true;
-            min_dist += dist;
-            for(auto it:adj[node]){
-                int node2 = it.first;
-                int dist2 = it.second;
-                if(!visited[node2]){
-                    pq.push({dist2,node2});
-                }
+        int min_sum = 0;
+        sort(edges.begin(),edges.end());
+        for(auto it:edges){
+            int d = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            int u_parent = find(u,parent);
+            int v_parent = find(v,parent);
+            if(u_parent != v_parent){
+                Union(u,v,parent);
+                min_sum += d;
             }
         }
-        return min_dist;
+        return min_sum;
     }
 };
