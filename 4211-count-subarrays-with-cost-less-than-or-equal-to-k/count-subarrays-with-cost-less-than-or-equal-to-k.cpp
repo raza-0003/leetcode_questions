bb110aa4cbe9d938fd7c7e8e3348527665f1e128
Faizan Raza <faizanraza2308@gmail.com>
2026-02-09 00:17:18 +0530

    Time: 519 ms (20.85%) | Memory: 300.9 MB (14.67%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        deque<int> mn,mx;
-        long long ans = 0, l = 0;
-        for(long long r = 0; r < nums.size(); r++){
-            while(!mn.empty() && nums[mn.back()] >= nums[r]) mn.pop_back();
-            while(!mx.empty() && nums[mx.back()] <= nums[r]) mx.pop_back();
-            mn.push_back(r);
-            mx.push_back(r);
-            while((1LL * (nums[mx.front()] - nums[mn.front()]) * (r-l + 1)) > k){
-                if(l == mn.front()) mn.pop_front();
-                if(l == mx.front()) mx.pop_front();;
-            l++;
+        multiset<int>s;
+        int i=0;
+        int j=0;
+        long long ans = 0;
+        int n = nums.size();
+        while(j < n){
+            s.insert(nums[j]);
+            while(i<= j && ((*s.rbegin()-(*s.begin())) * 1LL * s.size() > k)){
+                s.erase(s.find(nums[i]));
+                i++;
             }
-            ans += ((r-l)+1);
+            ans += (j-i+1);
+            j++;
         }
         return ans;
     }
