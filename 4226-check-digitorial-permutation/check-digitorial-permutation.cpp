fdf45aaf3ee08de290e169c5c766cdc614592975
Faizan Raza <faizanraza2308@gmail.com>
2026-02-23 20:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (88.24%) - LeetSync

--- 4226-check-digitorial-permutation/check-digitorial-permutation.cpp
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     int fact(int n){
-        if(n == 0 || n ==1 ){
+        if(n == 1 || n == 0){
             return 1;
         }
         return n * fact(n-1);
     }
-    bool isDigitorial(int n){
+     bool isPermutation(int a, int b) {
+        int freq[10] = {0};
+        while (a > 0) {
+            freq[a % 10]++;
+            a /= 10;
+        }
+        while (b > 0) {
+            freq[b % 10]--;
+            b /= 10;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] != 0) return false;
+        }
+        return true;
+    }
+    bool isDigitorialPermutation(int n) {
+        int mpp[10];
+        mpp[0] = 1;
+        mpp[1] = 1;
+        for(int i=2;i<=9;i++){
+            mpp[i] = fact(i);
+        }
         int m = n;
         int ans = 0;
         while(m){
             int digit = m % 10;
-            ans += fact(digit);
+            ans += mpp[digit];
             m = m/10;
         }
-        return ans == n;
-    }
-    bool isDigitorialPermutation(int n) {
-        string s = to_string(n);
-        sort(s.begin(),s.end());
-        do { 
-            if(s[0] == '0') continue;
-            int num = stoi(s);
-            if(isDigitorial(num)) return true;
-            
-        } while (next_permutation(s.begin(), s.end()));
-        return false;
+        return isPermutation(ans,n);
         
     }
 };
