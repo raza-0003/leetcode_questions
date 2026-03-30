c1a791c950453ca9bc182452f1781aa83edd105c
Faizan Raza <faizanraza2308@gmail.com>
2026-03-30 15:35:20 +0530

    Time: 6 ms (13.24%) | Memory: 16.3 MB (15.74%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int open,int close,string otp,vector<string>&ans){
+        if(open == 0 && close == 0){
+            // now its my ansl
+            ans.push_back(otp);
+            return;
+        }
+        if(open){
+            string otp1 = otp;
+            otp1.push_back('(');
+            solve(open-1,close,otp1,ans);
+        }
+        if(close > open){
+            string otp2 = otp;
+            otp2.push_back(')');
+            solve(open,close-1,otp2,ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string otp = "";
+        int open = n, close = n;
+        solve(open,close,otp,ans);
+        return ans;
+    }
+};
