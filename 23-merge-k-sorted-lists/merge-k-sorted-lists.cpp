297f31e37b36f497849bc8841bceae7408b91755
Faizan Raza <faizanraza2308@gmail.com>
2026-04-12 15:50:11 +0530

    Time: 1 ms (74.44%) | Memory: 18.5 MB (47.02%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,27 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+        else if(list2->val <= list1->val){
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+        return list1;
+        
+    }
+    ListNode* mergeAndUnion(vector<ListNode*>& lists,int s,int e){
+        if(s == e){
+            return lists[s];
+        }
+        int mid = s + (e-s)/2;
+        ListNode* L1 = mergeAndUnion(lists,s,mid);
+        ListNode* L2 = mergeAndUnion(lists,mid+1,e);
+        return mergeTwoLists(L1,L2);
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> ans;
-        // Step 1: Collect values
-        for (auto it : lists) {
-            while (it != nullptr) {
-                ans.push_back(it->val);
-                it = it->next;
-            }
-        }
-        // Edge case
-        if (ans.size() == 0) return nullptr;
-        // Step 2: Sort
-        sort(ans.begin(), ans.end());
-        // Step 3: Create linked list
-        ListNode* head = new ListNode(ans[0]);
-        ListNode* temp = head;
-
-        for (int i = 1; i < ans.size(); i++) {
-            temp->next = new ListNode(ans[i]);
-            temp = temp->next;   
-        }
-
-        return head;
+        int n = lists.size();
+        if(n == 0) return nullptr;
+        return mergeAndUnion(lists,0,n-1);
     }
 };
