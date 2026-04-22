23c8f642873be9bd1f77a6e259d057f62522dfb9
Faizan Raza <faizanraza2308@gmail.com>
2026-04-22 10:37:19 +0530

    Time: 810 ms (42.49%) | Memory: 180.4 MB (56.52%) - LeetSync

--- 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.cpp
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> findGoodIntegers(int n) {
         unordered_map<int,int>mpp;
         vector<int>ans;
-        for(int i=1;i<=cbrt(n);i++){
-            for(int j=i;j<cbrt(n);j++){
+        int x = cbrt(n);
+        for(int i=1;i<=x;i++){
+            for(int j=i;j<=x;j++){
                 long long sum = pow(i,3) + pow(j,3);
                 if(sum > n) break;
                 mpp[sum]++;
