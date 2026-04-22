9599dd293e30c0453efc7d53c42193854c119668
Faizan Raza <faizanraza2308@gmail.com>
2026-04-22 10:35:19 +0530

    Time: 787 ms (44.94%) | Memory: 180.3 MB (56.52%) - LeetSync

---
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findGoodIntegers(int n) {
+        unordered_map<int,int>mpp;
+        vector<int>ans;
+        for(int i=1;i<=cbrt(n);i++){
+            for(int j=i;j<cbrt(n);j++){
+                long long sum = pow(i,3) + pow(j,3);
+                if(sum > n) break;
+                mpp[sum]++;
+            }
+        }
+        for(auto [it,freq]:mpp){
+            if(freq >= 2){
+                ans.push_back(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
