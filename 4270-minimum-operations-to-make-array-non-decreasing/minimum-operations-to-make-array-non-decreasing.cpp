9324e32d5412779cf16db63953c2e389b7b23b0c
Faizan Raza <faizanraza2308@gmail.com>
2026-04-26 10:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 136.9 MB (65.83%) - LeetSync

---
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+        
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] < nums[i-1]) {
+                ans += (nums[i-1] - nums[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
