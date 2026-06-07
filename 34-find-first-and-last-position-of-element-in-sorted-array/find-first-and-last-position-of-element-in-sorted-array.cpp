3ab05093ad0936cb9947a968e5f94562520d308c
Faizan Raza <faizanraza2308@gmail.com>
2026-06-07 15:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.27%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        // lets find the first occr
+        int f_ind = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                f_ind = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        int l_ind = -1;
+        low = 0;
+        high = n-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                l_ind = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target){
+                low  = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        vector<int>ans;
+        ans.push_back(f_ind);
+        ans.push_back(l_ind);
+        return ans;
+
+        
+    }
+};
