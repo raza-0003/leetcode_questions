1c2d30a236b4f1a45cd562506db0cf363794fdd7
Faizan Raza <faizanraza2308@gmail.com>
2026-06-10 00:49:02 +0530

    Time: 58 ms (38.31%) | Memory: 61.6 MB (48.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool isPalindrome(int i,int j,string s){
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
+    bool isPalindrome(int start,int end,string s){
+        while(start<end){
+            if(s[start] != s[end]){
+                return 0;
             }
-            j--;
-            i++;
+            start++;
+            end--;
         }
         return true;
     }
-    void f(string s,int ind,vector<string>&path,vector<vector<string>>&res){
-        if(ind == s.size()){
-            res.push_back(path);
+    void solve(int index,string s,vector<string>&otp,vector<vector<string>>&ans){
+        if(index == s.size()){
+            ans.push_back(otp);
             return;
-        } 
-        for(int i=ind;i<s.size();i++){
-            if(isPalindrome(ind,i,s)){
-                path.push_back(s.substr(ind,i-ind+1));
-                f(s,i+1,path,res);
-                path.pop_back();
+        }
+        for(int parInd = index;parInd<s.size();parInd++){
+            if(isPalindrome(index,parInd,s)){
+                otp.push_back(s.substr(index,parInd-index+1));
+                solve(parInd+1,s,otp,ans);
+                otp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<vector<string>>res;
-        vector<string>path;
-        f(s,0,path,res);
-        return res;
+        vector<string>otp;
+        vector<vector<string>>ans;
+        solve(0,s,otp,ans);
+        return ans;
+        
     }
 };
