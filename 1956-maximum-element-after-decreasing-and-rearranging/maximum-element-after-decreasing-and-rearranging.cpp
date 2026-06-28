6c1e0ebe74d4b51b02368b8da941a1ad85d64041
Faizan Raza <faizanraza2308@gmail.com>
2026-06-28 10:30:38 +0530

    Time: 7 ms (74.58%) | Memory: 55.3 MB (36.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,12 +4,8 @@ public:
         int n = arr.size();
         int mx = 1;
         sort(arr.begin(),arr.end());
-        arr[0] = 1;
         for(int i=1;i<n;i++){
-            if(arr[i] > arr[i-1] +1){
-                arr[i] = arr[i-1] + 1;
-            }
-            mx = max(mx,arr[i]);
+            mx = min(arr[i],mx+1);
         }
         return mx;
         
