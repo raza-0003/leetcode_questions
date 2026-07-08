1f8baf745786c6afaf6979d9d3f6317fd936fa1f
Faizan Raza <faizanraza2308@gmail.com>
2026-07-08 17:14:26 +0530

    Time: 74 ms (31.09%) | Memory: 172.5 MB (23.32%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -4,50 +4,36 @@ using namespace std;
 class Solution {
 public:
     static const int MOD = 1e9 + 7;
-
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-
         vector<int> pos;
         vector<int> val;
-
         for (int i = 0; i < s.size(); i++) {
             if (s[i] != '0') {
                 pos.push_back(i);
                 val.push_back(s[i] - '0');
             }
         }
-
         int m = val.size();
-
         vector<long long> prefSum(m + 1, 0);
         vector<long long> prefNum(m + 1, 0);
         vector<long long> pow10(m + 1, 1);
-
         for (int i = 0; i < m; i++) {
             prefSum[i + 1] = prefSum[i] + val[i];
             prefNum[i + 1] = (prefNum[i] * 10 + val[i]) % MOD;
             pow10[i + 1] = (pow10[i] * 10) % MOD;
         }
-
         vector<int> ans;
-
         for (auto &q : queries) {
-
             int l = q[0];
             int r = q[1];
-
             int L = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
             int R = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
-
             if (L > R) {
                 ans.push_back(0);
                 continue;
             }
-
             int len = R - L + 1;
-
             long long sum = prefSum[R + 1] - prefSum[L];
-
             long long x =
                 (prefNum[R + 1] -
                  prefNum[L] * pow10[len] % MOD +
@@ -55,7 +41,6 @@ public:
 
             ans.push_back((x * sum) % MOD);
         }
-
         return ans;
     }
 };
