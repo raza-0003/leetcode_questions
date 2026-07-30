4876ffdfcc769e111e0a113b3df1798f126ec39c
Faizan Raza <faizanraza2308@gmail.com>
2026-07-30 11:14:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.80%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        if(n <= 8) return n;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            if(i<=8){
+                ans += 1;
+            }
+            else if(i > 8 && i <= 16){
+                ans += 2;
+            }
+            else if(i > 16 && i <= 24){
+                ans += 3;
+            }
+            else{
+                ans += 4;
+            }
+        }
+        return ans;
+    }
+};
