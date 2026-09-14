c3c199847261933a56a070c7a241d157ada80588
Faizan Raza <faizanraza2308@gmail.com>
2026-09-14 23:11:54 +0530

    Time: 1 ms (54.03%) | Memory: 13 MB (14.99%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        unordered_map<int,int>mpp;
+        int n = nums2.size();
+        int m = nums1.size();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
+                st.pop();
+            }
+            if(st.empty()) mpp[nums2[i]]=-1;
+            else{
+                mpp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
+        }
+        vector<int>ans;
+        for(int i=0;i<m;i++){
+            ans.push_back(mpp[nums1[i]]);
+        }
+        return ans;
+        
+    }
+};
