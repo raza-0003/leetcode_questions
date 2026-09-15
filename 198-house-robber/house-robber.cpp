368fb6462dffb2939326280f3f95407f1135f0c4
Faizan Raza <faizanraza2308@gmail.com>
2026-09-15 12:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.34%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //lets cover edges cases
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int oddsum = 0;
+        int evensum = 0;
+        for(int i=0;i<n;i++){
+            if(i%2 == 0){
+                evensum = max(oddsum,evensum+nums[i]); // taking even or skip it
+            }
+            else{
+                oddsum = max(evensum,nums[i]+oddsum);  // taking odd or skip it
+            }
+        }
+        return  max(oddsum,evensum);
+    }
+};
